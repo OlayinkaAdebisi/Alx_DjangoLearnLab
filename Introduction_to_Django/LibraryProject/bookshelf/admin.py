636d1a4da925e5e08d73e98c3e83828ad855eb2a
adebisi_olayinka <adebisio024@gmail.com>
2025-10-31 17:02:00 +0000

    2. Utilizing the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,4 +5,4 @@ admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author','publication_year')
-    list_filter = ('status', 'due_back')
+    list_filter = ('title', 'author','publication_year')
