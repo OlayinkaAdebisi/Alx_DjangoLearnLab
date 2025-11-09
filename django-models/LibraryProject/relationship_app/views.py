2dfd89fafd1cd9ef2f8cd374fc150c3827ba35a6
adebisi_olayinka <adebisio024@gmail.com>
2025-11-09 21:30:14 +0000

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.http import HttpResponseForbidden
+
 
 
 # Create your views here.
@@ -24,3 +28,27 @@ class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+    
+def is_admin(user):
+    if user.profile.role == 'Admin':
+        return user.profile.role == 'Admin'
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_member(user):
+    if user.profile.role == 'Member':
+        return user.profile.role == 'Member'
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+def is_librarian(user):
+    if user.profile.role == 'Librarian':
+        return user.profile.role == 'Librarian'
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
