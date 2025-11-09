603c8aa89f8f9b28f205b421861a2f20ad59498d
adebisi_olayinka <adebisio024@gmail.com>
2025-11-09 12:23:04 +0000

    0. Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from .models import Library
 from .models import Librarian
 books=Book.objects.filter(author=author_name)
 books.all()
-books=Library.objects.get(name = library_name)
+books=Library.objects.get(name=library_name)
 books.all()
 books=Librarian.objects.get(Library=library_name)
 books.all()
