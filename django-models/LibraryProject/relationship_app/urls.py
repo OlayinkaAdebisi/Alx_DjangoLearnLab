7dcdfb9273733146faaf28d4505ca461105a6a21
adebisi_olayinka <adebisio024@gmail.com>
2025-11-09 21:16:00 +0000

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import SignUpView
+from .views import register
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from .admin_view import admin_view
