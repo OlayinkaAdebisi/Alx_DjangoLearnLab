353269424a62700e2d2a5cef93cca1ec9893408e
adebisi_olayinka <adebisio024@gmail.com>
2025-11-15 14:34:17 +0000

    0. Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from .models import Book
-# Register your models here.
-admin.site.register(Book)
+from .models import Book,CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author','publication_year')
     list_filter = ('title', 'author','publication_year')
+class CustomUserAdmin(UserAdmin):
+    list_display = ["username", "date_of_birth", "profile_photo"]
+    list_filter = ('is_staff', 'is_superuser')
+    fieldsets = [
+        (None, {"fields": ["username", "password"]}),
+        ("Personal info", {"fields": ["email","profile_photo","date_of_birth"]}),
+        ("Permissions", {"fields": ["is_staff","is_superuser"]}),
+    ]
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
+# Register your models here.
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
