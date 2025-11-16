b9f6baa8485484a22208683b846aae886aa7df54
adebisi_olayinka <adebisio024@gmail.com>
2025-11-16 20:18:31 +0000

    2. Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import CustomUser, Book
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 # Create your views here.
 #it handles viewing
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -29,3 +32,23 @@ def delete_user(request, user_id):
     user.delete()
     return redirect("user_list")
 #book_list
+# views.py
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    
+    if request.method == 'POST':
+        if form.is_valid():  
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+            else:
+                form.add_error(None, "Invalid username or password")
+    
+    return render(request, 'login.html', {'form': form})
