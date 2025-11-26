68387b9a940aa9e9f21932370bb16f1e69f55569
adebisi_olayinka <adebisio024@gmail.com>
2025-11-26 16:58:42 +0100

    1. Building Custom Views and Generic Views in Django REST Framework

---
+++ advanced-api-project/api/README.md
@@ -0,0 +1,2 @@
+# Show details of one book
+# Public users can view it
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 # Create your views here.
+# creates DRF views
+class BookListView(generics.ListCreateAPIView):
+    queryset= Book.objects.all()
+    serializer_class = BookSerializer
+    # readonly for not authenticated
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    # readonly for not authenticated
+    permission_classes = [IsAuthenticatedOrReadOnly]
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    # provvides user authenitication and give crud
+    authentication_classes = [TokenAuthentication] 
+    permission_classes = [IsAuthenticated,IsAdminUser]
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    def get_queryset(self):
+        return Book.objects.filter(owner=self.request.user)
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = BookSerializer
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
