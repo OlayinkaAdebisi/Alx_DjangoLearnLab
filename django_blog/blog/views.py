4efba82a599a7579129f37f6b4d8479430f1f06e
adebisi_olayinka <adebisio024@gmail.com>
2025-12-02 14:59:44 +0100

    1. Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+# You can define the class right here
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 # Create your views here.
+#
+def signup(request):
+    return render(request,'blog/signup.html')
+#@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
