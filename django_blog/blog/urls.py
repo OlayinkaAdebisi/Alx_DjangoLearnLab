130b906fd9660836a9497cf0126a62be0b1cb629
adebisi_olayinka <adebisio024@gmail.com>
2025-12-03 14:40:26 +0100

    1. Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView,LogoutView
 from . import views
-from .views import signup
+#from .views import register
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/',views.signup,name='signup'),
+    path('register/',views.register,name='signup'),
     path('profile/',views.profile,name='profile'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -7,14 +7,24 @@ from django.contrib.auth.models import User
 # You can define the class right here
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 # Create your views here.
 #
 def register(request):
-    return render(request,'blog/signup.html')
-#@login_required
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)  # Use the custom form here
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()  # Use the custom form here too
+    return render(request, 'blog/signup.html', {'form': form})
+@login_required
 def profile(request):
     return render(request, 'blog/profile.html')
+#f
