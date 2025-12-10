6d641144183fd199f038939f98219d68da627923
adebisi_olayinka <adebisio024@gmail.com>
2025-12-10 14:10:00 +0100

    2. Implementing User Follows and Feed Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -8,7 +8,14 @@ class CustomUser(AbstractUser):
     followers= models.ManyToManyField(
         'self',
         symmetrical=False,
-        related_name='following'
+        related_name='followed_by',
+        #blank=True
+    )
+    following=models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followes',
+        blank=True
     )
     def __str__(self):
         return self.username
