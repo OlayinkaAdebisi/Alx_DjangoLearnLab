45040dd0e6de8b12d06588cdf19d3cdccec7518c
adebisi_olayinka <adebisio024@gmail.com>
2025-12-11 14:37:34 +0100

    3. Implementing Notifications and Likes Functionality

---
+++ social_media_api/notifications/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+"""from .models import Notification
+from posts.models import po
+from rest_framework import viewsets, permissions, status, generics
+# Create your views here.
+class liking(viewsets.ModelViewSet):
+    queryset=Notification.objects.all()
+    permission_classes=[permissions.IsAuthenticated]
+
+    def like(self,request):
+        post=self.get_object()"""
+
+
+
--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -17,3 +17,10 @@ class Comment(models.Model):
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+class Like(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
